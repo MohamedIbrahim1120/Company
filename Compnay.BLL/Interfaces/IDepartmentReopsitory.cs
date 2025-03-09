6f362ab1b6900048253e408e73271e6897498870
mo7m3d1235@outlook.com <mo7m3d1235@outlook.com>
2025-03-09 02:33:53 +0200

    Department Module BLL

---
+++ Compnay.BLL/Interfaces/IDepartmentReopsitory.cs
@@ -0,0 +1,24 @@
+﻿using Company.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compnay.BLL.Interfaces
+{
+    public interface IDepartmentReopsitory
+    {
+        IEnumerable<Department> GetAll();
+
+        Department? Get(int id);
+
+        int Add(Department model);
+
+        int Update(Department model);
+
+        int Delete(Department model);
+
+
+    }
+}
---
+++ Compnay.BLL/Repositories/DepartmentReopsitory.cs
@@ -0,0 +1,53 @@
+﻿using Company.DAL.Data.Contexts;
+using Company.DAL.Models;
+using Compnay.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compnay.BLL.Repositories
+{
+    public class DepartmentReopsitory : IDepartmentReopsitory
+    {
+        private readonly CompanyDbContext _context; // NULL
+
+        public DepartmentReopsitory()
+        {
+            _context = new CompanyDbContext();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+
+        public Department? Get(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+     
+      
+    }
+}
